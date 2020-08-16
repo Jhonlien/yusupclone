9225485225e156bda8dc684976e205b6b3b12ea9
fandi adhitya <fandiadhitya96@gmail.com>
2020-08-16 21:59:36 +0700

    add card data

--- src/screens/Explore.js
+++ src/screens/Explore.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import { StyleSheet, Text, View, ImageBackground } from 'react-native'
+import { StyleSheet, Text, View, ImageBackground, FlatList,ScrollView } from 'react-native'
 import Header from '../components/Header'
+import Card from '../components/Card'
 import Fontisto from 'react-native-vector-icons/Fontisto';
-
+import {useSelector} from 'react-redux'
 // const image = {uri: 'https://wallpapercave.com/wp/9GIKZfw.jpg'}
 //const image = {uri: 'https://thumbs.dreamstime.com/b/white-headphones-blue-background-123426136.jpg'}
 // const image = {uri: 'https://d39l2hkdp2esp1.cloudfront.net/img/photo/154505/154505_00_2x.jpg'}
 
 // const image = {uri: 'https://www.rd.com/wp-content/uploads/2019/10/newspaper-760x506.jpg'}
-
 
 const MiniCard = (props) =>{
     return (
@@ -39,9 +39,11 @@ const MiniCard = (props) =>{
 }
 
 const Explore = () => {
+    const CardData = useSelector(state=>state);
     return (
         <View style={{flex:1}}>
             <Header/>
+            <ScrollView>
             <View 
                 style={{
                     flexWrap:'wrap',
@@ -53,7 +55,21 @@ const Explore = () => {
                 <MiniCard image='https://d39l2hkdp2esp1.cloudfront.net/img/photo/154505/154505_00_2x.jpg' title="Games"/>
                 <MiniCard image='https://www.rd.com/wp-content/uploads/2019/10/newspaper-760x506.jpg' title="News" /> 
             </View>
+            <FlatList
+                data={CardData}
+                renderItem={({item})=>{
+                    return <Card
+                        videoId={item.id.videoId}
+                        title={item.snippet.title}
+                        thumbnail={item.snippet.thumbnails.default}
+                        channel={item.snippet.channelTitle}
+                    />
+                }}
+                keyExtractor={item => item.id.videoId}
+            />
+        </ScrollView>
         </View>
+        
     )
 }
 
